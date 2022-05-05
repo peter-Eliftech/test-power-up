78c257900a1c3bc88b7ffc3d988d7b74a93337b3
peter-Eliftech <peter.kachur@eliftech.com>
2022-05-05 15:11:04 +0300

    fix

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const Koa = require("koa");
 const serve = require("koa-static");
 const cors = require("@koa/cors");
-// const path = require("path");
+const path = require("path");
+
 const app = new Koa();
 
 // response
 app.use(cors({ origin: "https://trello.com" }));
-app.use(serve(__dirname + "/src/html/index.html"));
+app.use(serve(path.join(__dirname, "/src/html/")));
 
 app.listen(3000);
