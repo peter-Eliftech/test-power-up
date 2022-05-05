86c978b2eb254ecf6217aa397f03ce60f4178ed3
peter-Eliftech <peter.kachur@eliftech.com>
2022-05-05 16:14:11 +0300

    added options to card btns

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,14 @@ window.TrelloPowerUp.initialize({
   "card-buttons": function (t, opts) {
     return [
       {
+        // usually you will provide a callback function to be run on button click
+        // we recommend that you use a popup on click generally
+        icon: GRAY_ICON, // don't use a colored icon here
+        text: "Open Popup",
+        callback: onBtnClick,
+        condition: "edit",
+      },
+      {
         icon: GRAY_ICON,
         text: "Snooze",
         callback: function () {
