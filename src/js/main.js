90b525a7adcd055f04f0c9432ef19d7b4ff745e4
peter-Eliftech <peter.kachur@eliftech.com>
2022-05-05 16:22:38 +0300

    test btns

--- src/js/main.js
+++ src/js/main.js
@@ -9,58 +9,16 @@ window.TrelloPowerUp.initialize({
         // we recommend that you use a popup on click generally
         icon: GRAY_ICON, // don't use a colored icon here
         text: "Open Popup",
-        callback: () => {},
+        callback: () => console.log("lalla"),
         condition: "edit",
       },
       {
+        // but of course, you could also just kick off to a url if that's your thing
         icon: GRAY_ICON,
-        text: "Snooze",
-        callback: function () {
-          t.modal({
-            // the url to load for the iframe
-            url: "./html/modal.html",
-            // optional arguments to be passed to the iframe as query parameters
-            // access later with t.arg('text')
-            args: { text: "Hello" },
-            // optional color for header chrome
-            accentColor: "#F2D600",
-            // initial height for iframe
-            height: 500, // not used if fullscreen is true
-            // whether the modal should stretch to take up the whole screen
-            fullscreen: true,
-            // optional function to be called if user closes modal (via `X` or escape, etc)
-            callback: () => console.log("Goodbye."),
-            // optional title for header chrome
-            title: "appear.in meeting",
-            // optional action buttons for header chrome
-            // max 3, up to 1 on right side
-            actions: [
-              {
-                icon: "./images/icon.svg",
-                url: "https://google.com",
-                alt: "Leftmost",
-                position: "left",
-              },
-              {
-                icon: "./images/icon.svg",
-                callback: (tr) =>
-                  tr.popup({
-                    title: tr.localizeKey("appear_in_settings"),
-                    url: "settings.html",
-                    height: 164,
-                  }),
-                alt: "Second from left",
-                position: "left",
-              },
-              {
-                icon: "./images/icon.svg",
-                callback: () => console.log(":tada:"),
-                alt: "Right side",
-                position: "right",
-              },
-            ],
-          });
-        },
+        text: "Just a URL",
+        condition: "always",
+        url: "https://developer.atlassian.com/cloud/trello",
+        target: "Trello Developer Site", // optional target for above url
       },
     ];
   },
