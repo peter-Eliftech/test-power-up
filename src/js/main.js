1261132aeaf6f093663c977749f1201723f1238f
peter-Eliftech <peter.kachur@eliftech.com>
2022-05-05 15:48:52 +0300

    test options for power-up

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,33 @@
-TrelloPowerUp.initialize({
-  "card-buttons": function (t, options) {
+var GRAY_ICON =
+  "https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg";
+
+var btnCallback = function (t, opts) {
+  return t.popup({
+    title: "Snooze Card",
+    items: [
+      {
+        text: "Choose Time",
+        callback: function (t, opts) {},
+      },
+      {
+        text: "In 1 hour",
+        callback: function (t, opts) {},
+      },
+      {
+        text: "In 2 hours",
+        callback: function (t, opts) {},
+      },
+    ],
+  });
+};
+
+window.TrelloPowerUp.initialize({
+  "card-buttons": function (t, opts) {
     return [
       {
-        icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
-        text: "Estimate Size",
+        icon: GRAY_ICON,
+        text: "Snooze",
+        callback: btnCallback,
       },
     ];
   },
